bf3112f25b8aa0a1c403987aec29764e83aa218d
Antoine Boxho <karagan.app@gmail.com>
2017-01-09 14:46:19 +0100

    trying to fix out of gas issue

--- transactionUI/src/App.js
+++ transactionUI/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Web3 from 'web3';
@@ -11,18 +11,210 @@ var accounts = ETHEREUM_CLIENT.eth.accounts;
 ETHEREUM_CLIENT.eth.defaultAccount = accounts[0];
 
 // ABI and Address of contract living on the localhost
-var contractABI = [{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_money","type":"uint256"}],"name":"transferFromOwner","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getPeople","outputs":[{"name":"","type":"bytes32[]"},{"name":"","type":"bytes32[]"},{"name":"","type":"uint256[]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_personId","type":"address"},{"name":"_firstName","type":"bytes32"},{"name":"_lastName","type":"bytes32"}],"name":"addPerson","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getTransactions","outputs":[{"name":"","type":"address[]"},{"name":"","type":"address[]"},{"name":"","type":"uint256[]"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"peopleArray","outputs":[{"name":"personId","type":"address"},{"name":"firstName","type":"bytes32"},{"name":"lastName","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_money","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"transactionArray","outputs":[{"name":"sender","type":"address"},{"name":"receiver","type":"address"},{"name":"timestamp","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_personId","type":"address"}],"name":"getBalance","outputs":[{"name":"_balance","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[],"type":"constructor"}];
+var contractABI = [
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_to",
+        "type": "address"
+      }, {
+        "name": "_money",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFromOwner",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [],
+    "name": "getPeople",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bytes32[]"
+      }, {
+        "name": "",
+        "type": "bytes32[]"
+      }, {
+        "name": "",
+        "type": "uint256[]"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_personId",
+        "type": "address"
+      }, {
+        "name": "_firstName",
+        "type": "bytes32"
+      }, {
+        "name": "_lastName",
+        "type": "bytes32"
+      }
+    ],
+    "name": "addPerson",
+    "outputs": [
+      {
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [],
+    "name": "getTransactions",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address[]"
+      }, {
+        "name": "",
+        "type": "address[]"
+      }, {
+        "name": "",
+        "type": "uint256[]"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "peopleArray",
+    "outputs": [
+      {
+        "name": "personId",
+        "type": "address"
+      }, {
+        "name": "firstName",
+        "type": "bytes32"
+      }, {
+        "name": "lastName",
+        "type": "bytes32"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_from",
+        "type": "address"
+      }, {
+        "name": "_to",
+        "type": "address"
+      }, {
+        "name": "_money",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "transactionArray",
+    "outputs": [
+      {
+        "name": "sender",
+        "type": "address"
+      }, {
+        "name": "receiver",
+        "type": "address"
+      }, {
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "_personId",
+        "type": "address"
+      }
+    ],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "name": "_balance",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  }, {
+    "inputs": [],
+    "type": "constructor"
+  }
+];
 
 var contractAddress = '0x6788313e06a6b3e3645880aa5049e4ed1f55a0ac';
 
 // Intsance to contract
-var transactionsContract = ETHEREUM_CLIENT.eth.contract(contractABI).at(contractAddress);
+var transactionsContract = ETHEREUM_CLIENT
+  .eth
+  .contract(contractABI)
+  .at(contractAddress);
 
-// var sendEvent = transactionsContract.transfer({fromBlock: 0, toBlock: 'latest'});
+// var sendEvent = transactionsContract.transfer({fromBlock: 0, toBlock:
+// 'latest'}); convert strings into bytes
+const hexToByte = (hex) => {
+  for (var bytes = [], c = 0; c < hex.length; c += 2) {
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+  }
+  return bytes;
+}
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       firstNames: [],
@@ -37,7 +229,7 @@ class App extends Component {
     }
   }
 
-  componentWillMount(){
+  componentWillMount() {
     var data = transactionsContract.getPeople();
     var transactions = transactionsContract.getTransactions();
     this.setState({
@@ -50,53 +242,43 @@ class App extends Component {
     })
   }
 
-  _returnDate(uDate){
+  _returnDate(uDate) {
     var t = new Date(0);
     t.setUTCSeconds(uDate);
     var converted_date = T(t).format("DD/MM/YYYY - hh:MM:ss");
     return converted_date;
   }
 
+  //const address = ETHEREUM_CLIENT.eth.accounts[1];
+  _onHandleAddPerson = () => {
+    const address = this.state.enteredAddress;
+    const firstName = this.state.enteredFirstName;
+    const lastName = this.state.enteredLastName;
 
-    //const address = ETHEREUM_CLIENT.eth.accounts[1];
-    _onHandleAddPerson = () => {
-      const address = this.state.enteredAddress;
-      const firstName = this.state.enteredFirstName;
-      const lastName = this.state.enteredLastName;
-
-      // && address.length === 42
-      if (address !== "" && address.length === 42 && firstName !== "" && lastName !== ""){
-          transactionsContract.addPerson(address, firstName, lastName);
-          this.setState({
-            enteredAddress: "",
-            enteredFirstName: "",
-            enteredLastName: ""
-          });
-      }else{
-        console.log("Address not valid");
-      }
+    // && address.length === 42
+    if (address.length === 42 && firstName !== "" && lastName !== "") {
+      const bytesAddress = address
+      transactionsContract.addPerson(bytesAddress, firstName, lastName);
+      this.setState({enteredAddress: "", enteredFirstName: "", enteredLastName: ""});
+    } else {
+      console.log("Address not valid");
     }
+  }
 
-    onChangeAddress = (e) => {
-      const address = e.target.value;
-      this.setState({
-        enteredAddress: address
-      })
-    }
+  onChangeAddress = (e) => {
+    const address = e.target.value;
+    this.setState({enteredAddress: address})
+  }
 
-    onChangeFirstName = (e) => {
-      const firstName = e.target.value;
-      this.setState({
-        enteredFirstName: firstName
-      })
-    }
+  onChangeFirstName = (e) => {
+    const firstName = e.target.value;
+    this.setState({enteredFirstName: firstName})
+  }
 
-    onChangeLastName = (e) => {
-      const lastName = e.target.value;
-      this.setState({
-        enteredLastName: lastName
-      })
-    }
+  onChangeLastName = (e) => {
+    const lastName = e.target.value;
+    this.setState({enteredLastName: lastName})
+  }
 
   render() {
     var tableRows = [];
@@ -124,13 +306,25 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1>The Simplest Blockchain Model</h1>
         </div>
         <div className="App-input-container">
-          <input className="App-input" placeholder="Address" onChange={this.onChangeAddress} value={this.state.enteredAddress}/>
-          <input className="App-input" placeholder="Firstname" onChange={this.onChangeFirstName} value={this.state.enteredFirstName}/>
-          <input className="App-input" placeholder="Lastname" onChange={this.onChangeLastName} value={this.state.enteredLastName}/>
+          <input
+            className="App-input"
+            placeholder="Address"
+            onChange={this.onChangeAddress}
+            value={this.state.enteredAddress}/>
+          <input
+            className="App-input"
+            placeholder="Firstname"
+            onChange={this.onChangeFirstName}
+            value={this.state.enteredFirstName}/>
+          <input
+            className="App-input"
+            placeholder="Lastname"
+            onChange={this.onChangeLastName}
+            value={this.state.enteredLastName}/>
           <button className="App-button" onClick={this._onHandleAddPerson}>Add Person</button>
         </div>
         <div>
