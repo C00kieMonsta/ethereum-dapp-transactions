5749cdae9a818dd3afdd5b62ee9eb5e84e1d8338
Antoine <Karagan.app@gmail.com>
2017-01-09 16:40:51 +0100

    Adding owner permissions

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -33,8 +33,15 @@ contract Transactions{
     uint timestamp;
   }
 
+  // Adds conditions on who can execute the requested function
+  modifier onlyOwner {
+      if (msg.sender != owner)
+          throw;
+      _;
+  }
+
   // Adding a person to the contract with initial balance of 0
-  function addPerson(address _personId, bytes32 _firstName, bytes32 _lastName) returns (bool success){
+  function addPerson(address _personId, bytes32 _firstName, bytes32 _lastName) onlyOwner() returns (bool success) {
     Person memory newPerson;
     newPerson.personId = _personId;
     newPerson.firstName = _firstName;
@@ -95,7 +102,7 @@ contract Transactions{
   }
 
   // Transfer money from owner to other
-  function transferFromOwner(address _to, uint _money) returns(bool){
+  function transferFromOwner(address _to, uint _money) onlyOwner() returns(bool) {
     if (personToBalance[msg.sender] < _money){
       return false;
     }
@@ -123,34 +130,9 @@ contract Transactions{
     transactionArray.push(newTransaction);
     return true;
   }
+
+  // Method used to remove the contract from the blockchain, in case the contract gets buggy
+  function kill() onlyOwner() {
+    selfdestruct(owner);
+  }
 }
-
-/*
-    When sending money from one address to another, you get this to the log:
-
-    truffle(default)> contract.transfer(pers2, pers1, 100)
-    '0xa7edcc943002ea0406fe8742197bf637c0457841061c76e6427a13109e4e41cf'
-
-    This hash corresponds to the transaction between pers2 and pers1, you can access
-    the info about this transaction like this:
-
-      web3.eth.getTransaction("0xabce85e979001aa29bc36efbbb6d3b2f75849702e7587d9193691469738ff6af")
-
-    web3 is an object from web3 library that allows you to interact with the 'eth' or ethereum blockchain
-    through a bunch of mehods like 'getTransaction'. This is what you would get:
-
-    {
-      hash: '0xabce85e979001aa29bc36efbbb6d3b2f75849702e7587d9193691469738ff6af',
-      nonce: 7,
-      blockHash: '0x4f8a887e02090c2423153ebd17686ba8bda1e08e48a6471f66b7640eddeff080',
-      blockNumber: 8,
-      transactionIndex: 0,
-      from: '0x11989b1ad656a7a728bad8763d4a7c68b0fe7625',  // the owner of the contract is paying for the transaction cost
-      to: '0x336cc10032b4af8956777ab3ed2200bb4ea529e4', // Some random address
-      value: { [String: '0'] s: 1, e: 0, c: [ 0 ] },
-      gas: 4712388,
-      gasPrice: { [String: '100000000000'] s: 1, e: 11, c: [ 100000000000 ] },
-      input: '0x537e5e660000000000000000000000008d0fe3b090fd0185cb3ab1779ad57da30139fd8e4775696c6c61756d650000000000000000000000000000000000000000000000436172746f6e0000000000000000000000000000000000000000000000000000'
-    }
-
-*/
